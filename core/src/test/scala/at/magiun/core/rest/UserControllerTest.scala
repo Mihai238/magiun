675c4385a6aa2efa3b555680f219b853c4e0ad1b
Mihai Lepadat <mihailepadat@gmail.com>
2017-08-24 21:51:52 +0200

    fix test

--- core/src/test/scala/at/magiun/core/rest/UserControllerTest.scala
+++ core/src/test/scala/at/magiun/core/rest/UserControllerTest.scala
@@ -1,17 +1,22 @@
 package at.magiun.core.rest
 
+import at.magiun.core.MainModule
 import io.finch.Input
+import org.apache.spark.sql.SparkSession
 import org.junit.runner.RunWith
+import org.scalamock.scalatest.MockFactory
 import org.scalatest._
 import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class UserControllerTest extends FlatSpec with Matchers {
+class UserControllerTest extends FlatSpec with Matchers with MockFactory {
+
+  private val mainModule = new MainModule {}
 
   it should "test a simple test :)" in {
     val input = Input.get("/users/1")
 
-    val result = new UserController(null).getUser(input) // TODO
+    val result = new UserController(mainModule.spark).getUser(input)
 
     result.awaitValueUnsafe().get should be (UserDto("John"))
   }

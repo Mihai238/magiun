1c27e3d5f0e5edd8ef8b6bd0ede44782f9f72802
Mihai Lepadat <mihailepadat@gmail.com>
2017-11-05 12:02:59 +0100

    add routing

--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -2,22 +2,31 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
 import {AppComponent} from './app.component';
 import {DataService} from './services/data.service';
 import {DataComponent} from './components/data/data.component';
 import {LoggerModule, NgxLoggerLevel} from 'ngx-logger';
+import { AboutComponent } from './components/about/about.component';
+
+const appRoutes: Routes = [
+  {path: '', component: DataComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataComponent
+    DataComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    LoggerModule.forRoot({level: NgxLoggerLevel.INFO, serverLogLevel: NgxLoggerLevel.OFF})
+    LoggerModule.forRoot({level: NgxLoggerLevel.INFO, serverLogLevel: NgxLoggerLevel.OFF}),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     DataService

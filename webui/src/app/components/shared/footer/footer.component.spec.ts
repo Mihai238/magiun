956833ec3c4538b3dde146016bb345b205110b3a
Mihai Lepadat <mihailepadat@gmail.com>
2017-11-19 11:45:31 +0100

    fix test for footer

--- webui/src/app/components/shared/footer/footer.component.spec.ts
+++ webui/src/app/components/shared/footer/footer.component.spec.ts
@@ -1,4 +1,4 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, ComponentFixtureAutoDetect, TestBed} from '@angular/core/testing';
 import {FooterComponent} from './footer.component';
 import {translate} from '../../../app.translate';
 import {DebugElement} from '@angular/core';
@@ -9,19 +9,33 @@ describe('FooterComponent', () => {
   let comp: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [FooterComponent],
-      imports: [translate]
-    });
+      imports: [translate],
+      providers: [
+        {provide: ComponentFixtureAutoDetect, useValue: true}
+      ]
+    })
+      .compileComponents();
 
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(FooterComponent);
     comp = fixture.componentInstance;
   });
 
-  it('true is true', () => {
+  it('list includes 3 elements', () => {
     const debugElems: DebugElement[] = fixture.debugElement.queryAll(By.css('li'));
 
     expect(debugElems.length).toBe(3);
+  });
+
+  it('about entry is present', () => {
+    const debugElem: DebugElement = fixture.debugElement.query(By.css('li > a'));
+    const elem: HTMLElement = debugElem.nativeElement;
+
+    expect(elem.textContent).toContain('FOOTER.ABOUT');
   });
 });

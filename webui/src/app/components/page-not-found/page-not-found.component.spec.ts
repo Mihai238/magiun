368d8f1cd7de4d5d7f94dee2dfbc475035acc22d
Mihai Lepadat <mihailepadat@gmail.com>
2017-11-24 15:15:45 +0100

    make footer's position fixed

--- webui/src/app/components/page-not-found/page-not-found.component.spec.ts
+++ webui/src/app/components/page-not-found/page-not-found.component.spec.ts
@@ -1,7 +1,6 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {PageNotFoundComponent} from './page-not-found.component';
-import {translate} from '../../app.translate';
 import {TranslateModule} from '@ngx-translate/core';
 
 describe('PageNotFoundComponent', () => {

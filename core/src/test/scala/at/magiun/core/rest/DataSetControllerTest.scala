4d0fdc8f51af92ad976b68b826ba9d8e0c619bd8
Mihai Lepadat <mihailepadat@gmail.com>
2018-02-23 12:33:09 +0100

    backend: mock in test by overriding main module declarations

--- core/src/test/scala/at/magiun/core/rest/DataSetControllerTest.scala
+++ core/src/test/scala/at/magiun/core/rest/DataSetControllerTest.scala
@@ -1,21 +1,23 @@
 package at.magiun.core.rest
 
 import at.magiun.core.TestUtils._
-import at.magiun.core.UnitTest
 import at.magiun.core.service.DataSetService
+import at.magiun.core.{MainModule, UnitTest}
 import io.finch.Input
 
 import scala.concurrent.Future
 
 class DataSetControllerTest extends UnitTest {
 
+  private val mainModule = new MainModule {
+    override lazy val dataSetService: DataSetService = stub[DataSetService]
+  }
+
   it should "return a dataset" in {
     val input = Input.get("/datasets/1")
+    mainModule.dataSetService.find _ when 1 returns Future.successful(Option(testDs1))
 
-    val mockedService = stub[DataSetService]
-    mockedService.find _ when 1 returns Future.successful(Option(testDs1))
-
-    val result = new DataSetController(mockedService).getDataSet(input)
+    val result = mainModule.dataSetController.getDataSet(input)
 
     val dataSet = result.awaitValueUnsafe().get
     dataSet.id should be(1)

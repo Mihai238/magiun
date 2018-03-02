4dce568d6230dd216d00b9b9ded6ed49224fb736
Mihai Lepadat <mihailepadat@gmail.com>
2018-03-02 20:17:56 +0100

    backend: return empty schema if cannot connect to mongo

--- core/src/main/scala/at/magiun/core/connector/MongoDbConnector.scala
+++ core/src/main/scala/at/magiun/core/connector/MongoDbConnector.scala
@@ -1,28 +1,63 @@
 package at.magiun.core.connector
 
+import java.util.regex.Pattern
+
 import at.magiun.core.model.{Column, DataSetSource, Schema}
 import com.mongodb.spark.config.ReadConfig
 import com.mongodb.spark.sql._
+import com.mongodb.{BasicDBObject, MongoClient, MongoClientOptions}
 import com.typesafe.scalalogging.LazyLogging
 import org.apache.spark.sql.SparkSession
 
+import scala.util.control.NonFatal
+
 class MongoDbConnector(spark: SparkSession) extends Connector with LazyLogging {
 
+  private val CONNECTION_TIMEOUT = 2000
+  private val hostPattern = Pattern.compile("mongodb://([^/]*)/.*")
+
   override def getSchema(source: DataSetSource): Schema = {
-    val lastSlashIndex = source.url.lastIndexOf("/")
-    val mongoUri = source.url.substring(0, lastSlashIndex)
-    val collectionName = source.url.substring(lastSlashIndex + 1)
+    val url = source.url
+    val lastSlashIndex = url.lastIndexOf("/")
+    val mongoUri = url.substring(0, lastSlashIndex)
+    val collectionName = url.substring(lastSlashIndex + 1)
 
     logger.info(s"Connecting to mongo uri '$mongoUri'; collection '$collectionName'")
 
-    val readConfig = ReadConfig(Map("uri" -> mongoUri, "collection" -> collectionName))
-    val dataFrame = spark.read.mongo(readConfig)
+    if (isMongoReachable(url)) {
+      val readConfig = ReadConfig(Map("uri" -> mongoUri, "collection" -> collectionName))
+      val dataFrame = spark.read.mongo(readConfig)
 
-    val cols = dataFrame.schema.zipWithIndex.map { case (col, index) =>
-      Column(index, col.name, mapToColumnType(col.dataType))
+      val cols = dataFrame.schema.zipWithIndex.map { case (col, index) =>
+        Column(index, col.name, mapToColumnType(col.dataType))
+      }
+
+      Schema(cols.toList)
+    } else {
+      Schema(List.empty)
     }
+  }
 
-    Schema(cols.toList)
+  private def isMongoReachable(url: String) = {
+    val options = MongoClientOptions.builder()
+      .connectTimeout(CONNECTION_TIMEOUT)
+      .socketTimeout(CONNECTION_TIMEOUT)
+      .maxWaitTime(CONNECTION_TIMEOUT)
+      .serverSelectionTimeout(CONNECTION_TIMEOUT)
+      .build()
+
+    val m = hostPattern.matcher(url)
+    m.find()
+    val host = m.group(1)
+
+    val client = new MongoClient(host, options)
+
+    try {
+      client.getDatabase("soomeDb").runCommand(new BasicDBObject("ping", "1"))
+      true
+    } catch {
+      case NonFatal(e) => false
+    }
   }
 
 }

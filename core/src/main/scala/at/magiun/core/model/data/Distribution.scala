9e50e9d47fddd6f6c046e660e302e0c55008b99b
Paul Curcean <curcean.paul@gmail.com>
2018-11-20 21:15:23 +0100

    added new Distributions

--- core/src/main/scala/at/magiun/core/model/data/Distribution.scala
+++ core/src/main/scala/at/magiun/core/model/data/Distribution.scala
@@ -4,11 +4,15 @@ import enumeratum._
 
 import scala.collection.immutable
 
-sealed abstract class Distribution(val mean: Double, val variance: Double) extends EnumEntry
+sealed abstract class Distribution(val name: String) extends EnumEntry
 
 object Distribution extends Enum[Distribution] with CirceEnum[Distribution] {
 
-  case object Normal extends Distribution(0, 1)
+  case object Normal extends Distribution("Normal Distribution")
+  case object Uniform extends Distribution("Uniform Distribution")
+  case object Exponential extends Distribution("Exponential Distribution")
+  case object Log extends Distribution("Log Distribution")
+
 
   val values: immutable.IndexedSeq[Distribution] = findValues
 }
